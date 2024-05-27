1e42bb89ae3a72b9ecf3ee7ad56d56f60eefefbc
Rafael Sarilho <105075638+rafaelsarilho@users.noreply.github.com>
2024-05-27 14:02:50 -0300

    Alterações menu lateral mobile

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-function openNav() {
-    document.getElementById("sideMenu").style.width = "250px";
+function toggleMenu() {
+    const sideMenu = document.getElementById("sideMenu");
+    if (sideMenu.style.width === "250px") {
+        closeNav(); // Fecha o menu
+    } else {
+        openNav(); // Abre o menu
+    }
 }
 
 function closeNav() {
     document.getElementById("sideMenu").style.width = "0";
+}
+
+function openNav() {
+    document.getElementById("sideMenu").style.width = "250px";
 }
