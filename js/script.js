1663654b73357954f70cc2d371f20ee0508f6aad
Rafael Sarilho <105075638+rafaelsarilho@users.noreply.github.com>
2024-05-27 17:32:06 -0300

    alterações appbar

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ function closeNav() {
 function openNav() {
     document.getElementById("sideMenu").style.width = "250px";
 }
+
+// Adiciona um evento de clique para todos os links do menu
+const menuLinks = document.querySelectorAll('.menu-bar a');
+menuLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        // Verifica se o link é para o PDF
+        if (this.getAttribute('href') === 'pdfs/catalogo.pdf') {
+            // Link para o PDF, não impede o comportamento padrão
+            return; 
+        } else {
+            // Link para uma seção, impede o comportamento padrão
+            event.preventDefault(); 
+            const targetId = this.getAttribute('href'); 
+            const targetElement = document.querySelector(targetId); 
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
