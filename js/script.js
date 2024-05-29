03311f53707cd1971463274860ea88800b44a6db
Rafael Sarilho <105075638+rafaelsarilho@users.noreply.github.com>
2024-05-29 14:31:01 -0300

    adição do botão para visitar a loja e correções de bugs

--- js/script.js
+++ js/script.js
@@ -1,34 +1,30 @@
 function toggleMenu() {
     const sideMenu = document.getElementById("sideMenu");
-    if (sideMenu.style.width === "250px") {
-        closeNav(); 
-    } else {
-        openNav();
-    }
+    sideMenu.classList.toggle('open');
 }
 
 function closeNav() {
-    document.getElementById("sideMenu").style.width = "0";
+    document.getElementById("sideMenu").classList.remove('open');
 }
 
 function openNav() {
-    document.getElementById("sideMenu").style.width = "250px";
+    document.getElementById("sideMenu").classList.add('open');
 }
 
 // Adiciona um evento de clique para todos os links do menu
 const menuLinks = document.querySelectorAll('.menu-bar a');
 menuLinks.forEach(link => {
     link.addEventListener('click', function(event) {
-        // Verifica se o link é para o PDF
-        if (this.getAttribute('href') === 'pdfs/catalogo.pdf') {
-            // Link para o PDF, não impede o comportamento padrão
+        // Verifica se o link é para o PDF ou o site externo
+        if (this.getAttribute('href') === 'pdfs/catalogo.pdf' || this.getAttribute('href') === 'https://www.babamateriais.com.br/') {
+            // Não impede o comportamento padrão
             return; 
         } else {
             // Link para uma seção, impede o comportamento padrão
             event.preventDefault(); 
             const targetId = this.getAttribute('href'); 
             const targetElement = document.querySelector(targetId); 
-            targetElement.scrollIntoView({ behavior: 'smooth' });
+            targetElement.scrollIntoView({ behavior: 'smooth' }); 
         }
     });
 });
