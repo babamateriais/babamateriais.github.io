ed67c78e4b6a383078792a11ae084dc0e9677b0f
Rafael Sarilho <105075638+rafaelsarilho@users.noreply.github.com>
2024-06-03 15:57:33 -0300

    atualização catálogo pdf

--- js/script.js
+++ js/script.js
@@ -3,15 +3,24 @@ function toggleMenu() {
     sideMenu.classList.toggle('open');
 }
 
-function closeNav() {
-    document.getElementById("sideMenu").classList.remove('open');
-}
+// Adiciona um evento de clique para todos os links do menu lateral
+const sideMenuLinks = document.querySelectorAll('.side-menu a');
+sideMenuLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        if (this.getAttribute('href') === 'pdfs/catalogo.pdf' || this.getAttribute('href') === 'https://www.babamateriais.com.br/') {
+            // Não impede o comportamento padrão
+            return; 
+        } else {
+            event.preventDefault(); 
+            const targetId = this.getAttribute('href'); 
+            const targetElement = document.querySelector(targetId); 
+            targetElement.scrollIntoView({ behavior: 'smooth' }); 
+            toggleMenu(); // Fecha o menu lateral
+        }
+    });
+});
 
-function openNav() {
-    document.getElementById("sideMenu").classList.add('open');
-}
-
-// Adiciona um evento de clique para todos os links do menu
+// Adiciona um evento de clique para todos os links da barra de menu
 const menuLinks = document.querySelectorAll('.menu-bar a');
 menuLinks.forEach(link => {
     link.addEventListener('click', function(event) {
